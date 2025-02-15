a95de1dab8a5d8718afeb705e2f25ffc302723f4
Philipp Aurand <pwaurand@gmail.com>
2025-02-15 00:40:31 -0800

    Fix Math.random logic. Edit score announcements

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getComputerChoice(){
     const computerChoice = Math.random();
     if (computerChoice <= .33) {
         return "rock";
-    } else if (computerChoice >= .33) {
+    } else if (computerChoice >= .66) {
         return "paper";
     } else {
         return "scissor";
@@ -18,9 +18,10 @@ function getHumanChoice() {
     const humanChoice = prompt("Please enter rock, paper, scissor!");
     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissor") {
         return humanChoice;
-    } else (
-        alert("Invalid Choice! Enter rock, paper or scissor!")
-    )
+    } else {
+        alert("Invalid Choice! Enter rock, paper or scissor!");
+        return getHumanChoice();
+    }
 }
 console.log(getComputerChoice());
 
@@ -32,10 +33,10 @@ function playRound(humanChoice, computerChoice) {
                humanChoice === "paper" && computerChoice === "rock" ||
                humanChoice === "scissor" && computerChoice === "paper")  {
                humanScore++; 
-               console.log("You win!", humanScore);
+               console.log("You win the match!", humanScore);
     } else {
         computerScore++ ;
-        console.log("You loose!", computerScore);
+        console.log("You loose the match!", computerScore);
     }
 }
 const humanSelection = getHumanChoice();
@@ -51,10 +52,14 @@ function playGame() {
        playRound(humanSelection, computerSelection);
       
     } 
+
+    console.log(`The final score is - You: ${humanScore}, Computer: ${computerScore},`);
     if (humanScore > computerScore) {
         console.log("You win the best of 5 series!")
-    } else {
+    } else if (humanScore < computerScore) {
         console.log("You Loose the series:(")
+    } else {
+        console.log("It's a tie!")
     }
 }
 playGame();
